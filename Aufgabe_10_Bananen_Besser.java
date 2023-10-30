30e46f863dae9baf806ef01f5a9d8e76faa936bb
YayCover9000 <yassuanf@googlemail.com>
2023-10-30 09:59:41 +0100

    changed Bannanenrechner to give out free banana and not accept negative values

--- Aufgabe_10_Bananen_Besser.java
+++ Aufgabe_10_Bananen_Besser.java
@@ -24,6 +24,9 @@ public class Aufgabe_10_Bananen_Besser {
 		float mwSt = 7;
 		float mwStBetrag = 0;
 		String euroZeichen = "\u20AC";
+		boolean negativInput = false;
+		int anzahlGratisBananen = 0;
+		int zaehler = 0;
 		
 		System.out.println("Herzlich Willkommen bei JavaMarkt");
 		System.out.println();
@@ -34,25 +37,39 @@ public class Aufgabe_10_Bananen_Besser {
 		System.out.println("Bitte geben Sie das Gewicht der Banenen in Gramm ein:");
 		gewicht = sc.nextInt();
 		
-		// Berechne den Bruttopreis
-		// Gewicht in kg umrechnen, mit Kilopreis multiplizieren
-		bruttoPreis = (((float)gewicht/1000) * kiloPreis);
-		// Berechne den Nettopreis
-		// Der Bruttopreis enthält die MwSt, daher gilt brutto = netto * 1.07 => netto = brutto / 1.07 * 1 
-		nettoPreis = bruttoPreis / (100 + mwSt) * 100;
+		//sicherstellen das nicht negativ
+		if(kiloPreis <= 0 || gewicht <= 0) {
+			negativInput = true;
+		}
+		if(negativInput == false) {
+			// Berechne den Bruttopreis
+			// Gewicht in kg umrechnen, mit Kilopreis multiplizieren
+			bruttoPreis = (((float)gewicht/1000) * kiloPreis);
+			// Berechne den Nettopreis
+			// Der Bruttopreis enthält die MwSt, daher gilt brutto = netto * 1.07 => netto = brutto / 1.07 * 1 
+			nettoPreis = bruttoPreis / (100 + mwSt) * 100;
 
-		mwStBetrag = bruttoPreis - nettoPreis;
+			mwStBetrag = bruttoPreis - nettoPreis;
+			
+			// Ausgabe des Kassenzettels
+			System.out.println();
+			System.out.println("Ihre Quittung:");
+			// Was wurde gekauft?
+			System.out.println(((float)gewicht/1000) + " kg Bananen,\t" + ((float)kiloPreis / 100) + " €/kg");
+			// Brutto-Preis ausgeben
+			System.out.println("\tBrutto:\t" + (bruttoPreis / 100) + " " + euroZeichen + "/kg");
+			// enthaltene MwSt ausgeben
+			System.out.println("\tMWSt:\t" + (((float)Math.round(mwStBetrag) / 100)) + " €/kg");
+			// Netto-Preis ausgeben
+			System.out.println("\tNetto:\t" + ((float)Math.round(nettoPreis) / 100) + " €/kg"); 
+			if(bruttoPreis/100 >= 5) {
+				anzahlGratisBananen = (int)(bruttoPreis/100)/5;
+				System.out.println(anzahlGratisBananen + " Gratis Banane");
+			}
+		} else {
+			System.out.println("Eingabe negativ");
+		}
 		
-		// Ausgabe des Kassenzettels
-		System.out.println();
-		System.out.println("Ihre Quittung:");
-		// Was wurde gekauft?
-		System.out.println(((float)gewicht/1000) + " kg Bananen,\t" + ((float)kiloPreis / 100) + " €/kg");
-		// Brutto-Preis ausgeben
-		System.out.println("\tBrutto:\t" + (bruttoPreis / 100) + " " + euroZeichen + "/kg");
-		// enthaltene MwSt ausgeben
-		System.out.println("\tMWSt:\t" + (((float)Math.round(mwStBetrag) / 100)) + " €/kg");
-		// Netto-Preis ausgeben
-		System.out.println("\tNetto:\t" + ((float)Math.round(nettoPreis) / 100) + " €/kg");   
+		
 	}
 }
